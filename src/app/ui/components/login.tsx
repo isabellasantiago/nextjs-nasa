d9376864e5c4e486a3430c4c15122e439b9fa2dc
Isabella Santiago <isabella.-.bela@hotmail.com>
2024-03-31 23:14:35 -0300

    wip: signup

---
+++ src/app/ui/components/login.tsx
@@ -0,0 +1,18 @@
+import { poppins } from "../fonts";
+import Link from 'next/link'
+
+export default async function LoginComponent() {
+    return (
+        <div className="w-full h-full lg:w-2/5  max-w-xl bg-slate-900 flex flex-col justify-center items-center px-[5%] gap-6 ">
+            <h1 className={`${poppins.className} text-5xl antialiased font-bold text-white tracking-wide`}>
+                Login
+            </h1>
+            <input type="text" className="w-full h-12 bg-slate-800 rounded-lg border-solid border-2 border-gray-600 font-sans p-2 text-white" placeholder="example@email.com" />
+
+            <input type="password" className="w-full h-12 bg-slate-800 rounded-lg border-solid border-2 border-gray-600 font-sans p-2 text-white" placeholder="********" />
+
+            <button type="submit" className={`w-full h-12 bg-slate-200 hover:bg-slate-100 rounded-lg border-solid border border-gray-600 ${poppins.className} font-bold text-slate-900`}>Login</button>
+            <span className={`${poppins.className} text-gray-400 text-xs`}>Don't you have an account? <Link href="/signup" className="hover:text-gray-300">Sign up</Link></span>
+        </div>
+    )
+}
---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Ubuntu, Open_Sans, Bebas_Neue, Poppins } from 'next/font/google'
+
+export const ubuntu = Ubuntu({ subsets: ['latin'], weight: ["400", "500", "700"]})
+
+export const openSans = Open_Sans({ subsets: ['latin'], weight: ["400", "500", "700"]})
+
+
+export const bebasNeue = Bebas_Neue({ subsets: ['latin'], weight: ["400"]})
+
+export const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600", "700"]})
